b8c66b1eb8d8f88e6a5dc0101603751981aed45d
jodi <128635337+jodi-j@users.noreply.github.com>
2024-04-16 22:16:44 -0400

    add fragment functionality

--- app/src/main/java/com/example/cis436_project4/MainActivity.kt
+++ app/src/main/java/com/example/cis436_project4/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.cis436_project4
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.cis436_project4.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -14,5 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.item_1 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, SwipingFragment())
+                        .commit()
+                    true
+                }
+                R.id.item_2 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, ProductsFragment())
+                        .commit()
+                    true
+                }
+                R.id.item_3 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, ProfileFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
 }
